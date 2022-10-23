5dfb4f6b507ce03a98371656ae09b7d1aa026a51
Krys Frost <librarykrystal@gmail.com>
2022-10-22 21:39:31 -0500

    One stretch goal completed.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -97,10 +97,17 @@ console.log('Is it in the itinerary?', find('Tate Modern'));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if (letter == string.charAt(0)){
+    console.log(letter);
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
