fed025da0ef61a2323befc0e0ee17e165bed04ba
Krys Frost <librarykrystal@gmail.com>
2022-11-04 23:43:09 -0500

    Stretch #10 completed (with notes about what I tried first that didn't work).

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -125,6 +125,7 @@ function isFirstLetter(letter, string) {
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
+
 // At time of initial assignment submission, stretch #8 was completed,
 // #9 was partial, and #s 10 and 11 were not touched.
 // Completing all stretches as part of assignment re-submission.
@@ -142,10 +143,35 @@ function sumAll(numbers){
 console.log(sumAll(numberArray));
 
 
-
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+const numArray = [6, -2, 1, -7, 2, 0];
+
+function positivesOnly(numbersInput){
+  return numbersInput.filter(numby => numby>0);
+}
+console.log(positivesOnly(numArray));
+
+// Solution above works.
+// (And uses .filter() which I just learned about earlier today - neat!)
+// But I originally tried a long-form solution (below) that didn't work.
+// I see the issue:
+// that "numby" was referring to place rather than number value in that place,
+// But I did not know how to make it refer to number value instead
+// in order to evaluate the values in the array rather than their [i] location.
+
+// function positivesOnly(numbersInput){
+//   let posOnlyArray = [];
+//   // TEST console.log(posOnlyArray);
+//   for(let numby of numbersInput){
+//     if(numby==0){
+//       posOnlyArray.push(numbersInput[numby]);
+//     }
+//   }
+//   return posOnlyArray;
+// }
+// console.log(positivesOnly(numArray));
 
 
 
